eef63d8dc06535647c3924d0dc741bc49adb5e28
Abbos29 <abbosvaliev2003@gmail.com>
2025-07-25 21:08:45 +0500

    feat: add header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './reset.css'
+import './App.css'
+import '/public/fonts/stylesheet.css'
+import Header from './components/layout/Header/Header'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
